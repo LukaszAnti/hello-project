ecc3862e1be2e85d59b39c7459ab2f657d3e1a1a
Łukasz Antas <anti@MacBook-Air-ukasz.local>
2024-10-20 20:34:47 +0200

    poprawki

--- infrastructure/externaldns/main.tf
+++ infrastructure/externaldns/main.tf
@@ -10,24 +10,16 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_dns_managed_zone" "primary" {
-  name        = "app-portfolio-tech-zone"
-  dns_name    = "app-portfolio.tech."
-  description = "Managed zone for app-portfolio.tech"
-
-  visibility = "public"
-
-  project = var.project_id
+resource "google_dns_managed_zone" "flask_project_zone" {
+  name     = "flask-project-org-zone"
+  dns_name = "flask-project.org."  
+  description = "Managed zone for flask-project.org"
 }
 
-resource "google_dns_record_set" "app_a_record" {
-  name         = "app-portfolio.tech."
-  managed_zone = google_dns_managed_zone.primary.name
+resource "google_dns_record_set" "flask_project_a_record" {
+  name         = "flask-project.org."
+  managed_zone = google_dns_managed_zone.flask_project_zone.name
   type         = "A"
   ttl          = 300
-
-  rrdatas = [var.loadbalancer_ip]
-
+  rrdatas      = ["34.118.118.100"]  
 }
-
-
