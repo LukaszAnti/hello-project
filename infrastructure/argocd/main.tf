07f2a9259d4a8b56d0e716cd9fd95ff21a99fbea
Łukasz Antas <anti@macbook-air-ukasz.home>
2024-10-22 16:15:36 +0200

    szlify

--- infrastructure/argocd/main.tf
+++ infrastructure/argocd/main.tf
@@ -1,4 +1,3 @@
-# Fetch GKE Cluster state
 data "terraform_remote_state" "gke" {
   backend = "local"
   config = {
@@ -6,14 +5,12 @@ data "terraform_remote_state" "gke" {
   }
 }
 
-# Kubernetes Provider Configuration
 provider "kubernetes" {
   host                   = "https://${data.terraform_remote_state.gke.outputs.gke_cluster_endpoint}"
   cluster_ca_certificate = base64decode(data.terraform_remote_state.gke.outputs.gke_cluster_ca_certificate)
   token                  = data.terraform_remote_state.gke.outputs.gke_token
 }
 
-# Helm Provider Configuration
 provider "helm" {
   kubernetes {
     host                   = "https://${data.terraform_remote_state.gke.outputs.gke_cluster_endpoint}"
@@ -22,14 +19,12 @@ provider "helm" {
   }
 }
 
-# Create ArgoCD namespace
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd"
   }
 }
 
-# Helm Release for ArgoCD
 resource "helm_release" "argocd" {
   depends_on = [kubernetes_secret.argocd_repo_secret]
   name       = "argocd"
@@ -54,13 +49,11 @@ EOF
   ]
 }
 
-# Fetch SSH private key from Google Secret Manager
 data "google_secret_manager_secret_version" "ssh_private_key" {
   secret  = "github-ssh-private-key"
   project = var.project_id
 }
 
-# Kubernetes Secret for ArgoCD Repository credentials
 resource "kubernetes_secret" "argocd_repo_secret" {
   depends_on = [data.google_secret_manager_secret_version.ssh_private_key]
   metadata {
